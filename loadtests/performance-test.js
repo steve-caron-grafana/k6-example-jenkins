4cfec3a10c679f57308d4c72dec1c655df29b662
steve-caron-grafana <steve.caron@grafana.com>
2023-02-09 18:37:26 -0500

    stuff

--- loadtests/performance-test.js
+++ loadtests/performance-test.js
@@ -5,11 +5,16 @@ export let options = {
   duration: "1m",
   vus: 50,
   thresholds: {
-    http_req_duration: ["p(95)<500"] // 95 percent of response times must be below 500ms
+    http_req_duration: ["p(95)<2000"] // 95 percent of response times must be below 2 seconds
   }
 };
 
 export default function() {
-  http.post("http://34.29.70.21/carts/1/items", {"itemId":"03fef6ac-1896-4ce8-bd69-b798f85c6e0b", "unitPrice":"99.99"});
+
+  let data = {"itemId":"03fef6ac-1896-4ce8-bd69-b798f85c6e0b", "unitPrice":"99.99"}
+
+  http.post("http://34.29.70.21/carts/1/items", JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json' },
+  });
   sleep(3);
 };
